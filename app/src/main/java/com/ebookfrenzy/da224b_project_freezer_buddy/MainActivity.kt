f6cafaac063c7f2c92d1b7bd1f3db9e879c9188e
Axellllll2 <axel.jiwestam@gmail.com>
2021-02-22 10:55:56 +0100

    First commit.

--- app/src/main/java/com/ebookfrenzy/da224b_project_freezer_buddy/MainActivity.kt
+++ app/src/main/java/com/ebookfrenzy/da224b_project_freezer_buddy/MainActivity.kt
@@ -1,11 +1,73 @@
 package com.ebookfrenzy.da224b_project_freezer_buddy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ArrayAdapter
+import com.ebookfrenzy.da224b_project_freezer_buddy.R
+
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_first.*
+import java.util.ArrayList
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var listItems = ArrayList<String>()
+    var adapter: ArrayAdapter<String>? = null
+    var undoOnClickListener: View.OnClickListener = View.OnClickListener { view ->
+        listItems.removeAt(listItems.size - 1)
+        adapter?.notifyDataSetChanged()
+        Snackbar.make(view, "Item removed", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+    }
+
+    private fun addListItem() {
+        val dateFormat: SimpleDateFormat =
+                SimpleDateFormat("HH:mm:ss MM/dd/yyyy", Locale.US)
+        listItems.add(dateFormat.format(Date()))
+        adapter?.notifyDataSetChanged()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            addListItem()
+            Snackbar.make(view, "Item added to list", Snackbar.LENGTH_LONG)
+                    .setAction("Undo", undoOnClickListener).show()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                listItems)
+
+        listView.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
